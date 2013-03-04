00d4ddfa6c9b93d50d37d70c029ceb99eb243ead
Alan Witkowski <alan.witkowski@gmail.com>
2013-03-03 20:09:49 -0700

    added irrlicht version check

--- FindIrrlicht.cmake
+++ FindIrrlicht.cmake
@@ -1,12 +1,13 @@
 #	Alan Witkowski - CMake module to find irrlicht
 #
 #	Input:
-# 		IRRLICHT_ROOT - Environment variable that points to  irrlicht root directory
+# 		IRRLICHT_ROOT - Environment variable that points to the irrlicht root directory
 #
 #	Output:
 #		IRRLICHT_FOUND - Set to true if irrlicht was found
 #		IRRLICHT_INCLUDE_DIRS - Path to irrlicht.h
 #		IRRLICHT_LIBRARIES - Contains the list of irrlicht libraries
+#		IRRLICHT_VERSION_STRING - Version of irrlicht
 #
 
 set(IRRLICHT_FOUND false)
@@ -38,9 +39,16 @@ find_library(
 		lib/Win32-visualstudio
 )
 
+# get irrlicht version
+if(IRRLICHT_INCLUDE_DIRS AND EXISTS "${IRRLICHT_INCLUDE_DIRS}/IrrCompileConfig.h")
+	file(STRINGS "${IRRLICHT_INCLUDE_DIRS}/IrrCompileConfig.h" irrlicht_version_str REGEX "^#define[ \t]+IRRLICHT_SDK_VERSION[ \t]+\".+\"")
+	string(REGEX REPLACE "^#define[ \t]+IRRLICHT_SDK_VERSION[ \t]+\"([^\"]+)\".*" "\\1" IRRLICHT_VERSION_STRING "${irrlicht_version_str}")
+	unset(irrlicht_version_str)
+endif()
+
 # handle QUIET and REQUIRED
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Irrlicht REQUIRED_VARS IRRLICHT_LIBRARIES IRRLICHT_INCLUDE_DIRS)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Irrlicht REQUIRED_VARS IRRLICHT_LIBRARIES IRRLICHT_INCLUDE_DIRS VERSION_VAR IRRLICHT_VERSION_STRING)
 
 # advanced variables only show up in gui if show advanced is turned on
 mark_as_advanced(IRRLICHT_INCLUDE_DIRS IRRLICHT_LIBRARIES)
