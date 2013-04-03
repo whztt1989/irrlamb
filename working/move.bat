e58ccace62e08abbe030f6d627f8949b3ebbc7f8
Alan Witkowski <alan.witkowski@gmail.com>
2013-04-02 20:38:54 -0600

    finished batch file

--- working/move.bat
+++ working/move.bat
@@ -1,1 +1,6 @@
-for %%a IN (.\*.irr) do move %%a "levels/"%%~na 
+for %%a IN (.\*.obj) do call "..\assets\collision\colmesh.exe" %%a
+for %%a IN (.\*.irr) do move /Y %%a "levels/"%%~na 
+for %%a IN (.\*.col) do move /Y %%a "levels/"%%~na 
+
+del *.obj
+del irrb.log
