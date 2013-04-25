6ce2ce06cd7cf552df08310f0085498f0eebe087
Alan Witkowski <alan.witkowski@gmail.com>
2013-04-24 21:17:07 -0600

    worked on roll0

--- working/levels/c_roll0/c_roll0.lua
+++ working/levels/c_roll0/c_roll0.lua
@@ -10,9 +10,14 @@ end
 tConstraint = Level.GetTemplate("constraint_z")
 tLog = Level.GetTemplate("log")
 
-oLog = Level.CreateObject("log0", tLog, 2, 1.25, 1.75, 90, 0, 0)
---Level.CreateConstraint("constraint0", tConstraint, oLog, 0)
+oLog = Level.CreateObject("log0", tLog, 2, 1.25, 2.5, 90, 0, 0)
 Object.SetAngularVelocity(oLog, 0, 0, 1)
 
+oLog = Level.CreateObject("log1", tLog, -20, 1.25, 10.5, 90, 0, 0)
+Object.SetAngularVelocity(oLog, 0, 0, 0)
+
+oLog = Level.CreateObject("log2", tLog, 2, 1.25, 18.5, 90, 0, 0)
+Object.SetAngularVelocity(oLog, 0, 0, 0)
+
 -- Set up goal
-GoalCount = 1
+GoalCount = 3
