eabe50253a85be141bfb561ab5b40534a26a9f2b
Alan Witkowski <alan.witkowski@gmail.com>
2013-05-12 17:12:26 -0600

    removed old colmesh.exe

--- assets/collision/convert.bat
+++
@@ -1,5 +0,0 @@
-for %%a IN (..\scenes\*.obj) do call colmesh.exe %%a
-xcopy /Y ..\scenes\*.col ..\..\working\collision
-del ..\scenes\*.obj
-del ..\scenes\*.mtl
-del ..\scenes\*.col
--- working/move.bat
+++ working/move.bat
@@ -1,4 +1,4 @@
-for %%a IN (.\*.obj) do call "..\assets\collision\colmesh.exe" %%a
+for %%a IN (.\*.obj) do call "..\bin\Release\colmesh.exe" %%a
 for %%a IN (.\meshes\*.irrmesh) do call "..\assets\irrb\imeshcvt.exe" -i %%a -o meshes\%%~na.irrbmesh
 for %%a IN (.\*.irr) do move /Y %%a levels\%%~na 
 for %%a IN (.\*.col) do move /Y %%a levels\%%~na 
