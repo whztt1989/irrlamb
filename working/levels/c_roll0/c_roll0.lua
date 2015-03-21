dba6b78b4ecc8015e8b3ee62b43a5277c69bb02b
Alan Witkowski <alan.witkowski@gmail.com>
2015-03-20 18:00:07 -0600

    fixed floor on c_roll0

--- working/levels/c_roll0/c_roll0.lua
+++ working/levels/c_roll0/c_roll0.lua
@@ -13,15 +13,33 @@ function OnHitZone(HitType, Zone, HitObject)
 		Level.Lose()
 		return 1
 	else
-		Object.SetLifetime(HitObject, 2);
+		Object.SetLifetime(HitObject, 2)
 	end
 	
 	return 0
 end
 
 -- Set up level
-tConstraint = Level.GetTemplate("constraint_z")
+tFloor = Level.GetTemplate("floor")
 tLog = Level.GetTemplate("log")
+
+Z = 0.25
+oFloor = Level.CreateObject("floor", tFloor, -12.5, -0.25, Z, 0, 0, 0)
+
+Z = Z + 4.5
+oFloor = Level.CreateObject("floor", tFloor, -12.5, -0.25, Z, 0, 0, 0)
+
+Z = Z + 3.5
+oFloor = Level.CreateObject("floor", tFloor, -12.5, -0.25, Z, 0, 0, 0)
+
+Z = Z + 4.5
+oFloor = Level.CreateObject("floor", tFloor, -12.5, -0.25, Z, 0, 0, 0)
+
+Z = Z + 3.5
+oFloor = Level.CreateObject("floor", tFloor, -12.5, -0.25, Z, 0, 0, 0)
+
+Z = Z + 4.5
+oFloor = Level.CreateObject("floor", tFloor, -12.5, -0.25, Z, 0, 0, 0)
 
 oLog = Level.CreateObject("log0", tLog, 2, 1.25, 2.5, 90, 0, 0)
 Object.SetAngularVelocity(oLog, 0, 0, 0)
